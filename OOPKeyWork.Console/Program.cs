b5696d65ece9b259e2b74f48decac23323a04cb3
Sakhawat hosen <sakhawatcmt@gmail.com>
2020-02-25 16:58:06 +0600

    Params Key word Example

--- OOPKeyWork.Console/Program.cs
+++ OOPKeyWork.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace OOPKeyWork.Console
 {
@@ -9,12 +10,35 @@ namespace OOPKeyWork.Console
         {
             IMember member = new Teacher("", "", "", "", "", "", "", "");
 
-
             Teacher aTeacher = member as Teacher;
 
-            aTeacher.userMailCheck();
+            aTeacher.userMailCheck("", "");
 
+            var sum = Sum(new int[] { 1, 2, 3 });
+            sum = Sum(2, 5, 6, 7);
+            sum = Sum(4, 5, 4, 5, 6, 7);
+
+            UserParam(1, 2, 3, 4);
 
         }
+
+        public static int Sum(params int[] items)
+        {
+            var sum = 0;
+            foreach (var item in items)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        public static void UserParam(params int[] lits)
+        {
+            for (int i = 0; i < lits.Length; i++)
+            {
+                System.Console.WriteLine(lits[i]);
+            }
+        }
+    }
     }
 }
